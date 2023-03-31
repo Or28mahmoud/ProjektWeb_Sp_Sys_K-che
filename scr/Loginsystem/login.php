0ff1c1f741353c3ec829743ee3abdbe7c8572450
Or28mahmoud <85789223+Or28mahmoud@users.noreply.github.com>
2023-03-31 20:53:12 +0200

    edit

---
+++ scr/Loginsystem/login.php
@@ -0,0 +1,36 @@
+<?php 
+ require_once("../Loginsystem/Configuration.php");
+
+    function login($user_username,$password_user){
+     
+     $com = getConnector();
+    $stmt = $com->prepare("SELECT user_id,user_name,user_username,password_user
+     from User where user_username =:user_username");
+    $stmt->bindParam(":user_username",$user_username);
+
+    $stmt->execute();
+    $userda = $stmt->fetchAll();
+
+   
+   
+    
+    $user_username1= $userda[0]['user_username'];
+    $pass =          $userda[0]['password_user'];
+    
+    if($user_username == $user_username1){
+        
+        if($password_user ==$pass){
+            
+             echo "sie sind eingeloggt";
+            // require("../ProjektWeb_Sp_Sys_K-che/scr/Hauptseite.html");
+        }else{
+            echo"Password ist falsch eingetregen";
+        }}
+        else{
+           echo "Username ist falsch eingetragen ";
+        }  
+    } 
+  login($_POST['user_username'],$_POST['password_user']);   
+   
+
+?>
